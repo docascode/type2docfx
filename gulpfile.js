1cf3998490b881cfb881e3fd67102ae88d6292a4
Mihail Smaga <Mihail.Smaga@zeppelin.com>
2018-12-29 11:00:20 +0200

    remove sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tslint = require("gulp-tslint");
 var tsProject = ts.createProject("tsconfig.json");
-var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task("default", function () {
     return tsProject.src()
@@ -13,9 +12,6 @@ gulp.task("default", function () {
             formatter: "verbose"
         }))
         .pipe(tslint.report())
-        .pipe(sourcemaps.init())
         .pipe(tsProject())        
-        .js
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest("dist"));
+        .js.pipe(gulp.dest("dist"));
 });
