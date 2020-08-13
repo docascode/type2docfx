29a2ea53305aef254016050df694db0cba9784ea
Martin <7252614+Lhoerion@users.noreply.github.com>
2020-08-13 13:02:57 +0200

    Refactor accessor generation

--- src/converters/accessor.ts
+++ src/converters/accessor.ts
@@ -3,24 +3,33 @@ import { Node } from '../interfaces/TypeDocModel';
 import { AbstractConverter } from './base';
 import { typeToString } from '../idResolver';
 import { Context } from './context';
+import { langs } from '../common/constants';
 
 export class AccessorConverter extends AbstractConverter {
     protected generate(node: Node, context: Context): Array<YamlModel> {
         const uid = context.ParentUid + '.' + node.name;
         console.log(` - ${node.kindString}: ${uid}`);
-        let signatureType;
+        let signatureTypeGet;
+        let signatureTypeGetBody;
+        let signatureTypeSet;
+        let signatureTypeSetBody;
         if (node.getSignature) {
             if (Array.isArray(node.getSignature)) {
-                signatureType = node.getSignature[0].type;
+                signatureTypeGet = node.getSignature[0].type;
             } else {
-                signatureType = node.getSignature.type;
+                signatureTypeGet = node.getSignature.type;
             }
-        } else if (node.setSignature) {
+            let signatureTypeGetParams = this.fillParameters((Array.isArray(node.getSignature) ? node.getSignature[0] : node.getSignature).parameters);
+            signatureTypeGetBody = this.generateCallFunction(`get ${node.name}`, signatureTypeGetParams, []);
+        }
+        if (node.setSignature) {
             if (Array.isArray(node.setSignature)) {
-                signatureType = node.setSignature[0].type;
+                signatureTypeSet = node.setSignature[0].type;
             } else {
-                signatureType = node.setSignature.type;
+                signatureTypeSet = node.setSignature.type;
             }
+            let signatureTypeSetParams = this.fillParameters((Array.isArray(node.setSignature) ? node.setSignature[0] : node.setSignature).parameters);
+            signatureTypeSetBody = this.generateCallFunction(`set ${node.name}`, signatureTypeSetParams, []);
         }
 
         const model: YamlModel = {
@@ -28,17 +37,27 @@ export class AccessorConverter extends AbstractConverter {
             name: node.name,
             fullName: node.name,
             children: [],
-            langs: ['typeScript'],
+            langs: langs,
             type: 'property',
             summary: node.comment ? this.findDescriptionInComment(node.comment) : '',
             syntax: {
-                content: `${node.flags && node.flags.isStatic ? 'static ' : ''}${typeToString(this.extractType(signatureType)[0])} ${node.name}`,
+                content: '',
                 return: {
-                    type: this.extractType(signatureType),
+                    type: signatureTypeGet ? this.extractType(signatureTypeGet) : this.extractType(signatureTypeSet),
                     description: this.extractReturnComment(node.comment)
                 }
             }
         };
+
+        if (signatureTypeGet) {
+            model.syntax.content += `${node.flags && node.flags.isStatic ? 'static ' : ''}${signatureTypeGetBody}: ${typeToString(this.extractType(signatureTypeGet)[0])}`;
+        }
+        if (signatureTypeGet && signatureTypeSet) {
+            model.syntax.content += '\n';
+        }
+        if (signatureTypeSet) {
+            model.syntax.content += `${node.flags && node.flags.isStatic ? 'static ' : ''}${signatureTypeSetBody}: ${typeToString(this.extractType(signatureTypeSet)[0])}`;
+        }
 
         return [model];
     }
