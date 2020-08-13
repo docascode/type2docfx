3e925bf09e9820aa1d0824c0d3f5852797eeae38
Martin <7252614+Lhoerion@users.noreply.github.com>
2020-08-13 11:52:58 +0200

    Remove type alias from top level items

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,4 +1,4 @@
 export const constructorName = 'constructor';
 export const yamlHeader = '### YamlMime:UniversalReference';
-export const setOfTopLevelItems = new Set(['class', 'interface', 'module', 'type alias']);
+export const setOfTopLevelItems = new Set(['class', 'interface', 'module']);
 export const langs = ['typeScript'];
