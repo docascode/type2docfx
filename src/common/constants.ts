7a7c182821c79de012d2f4e0018beccd1764138e
yunair <airzhaoyn@gmail.com>
2020-09-14 13:52:50 +0800

    add constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,3 +2,5 @@ export const constructorName = 'constructor';
 export const yamlHeader = '### YamlMime:UniversalReference';
 export const setOfTopLevelItems = new Set(['class', 'interface', 'module', 'type alias']);
 export const langs = ['typeScript'];
+export const sdpYamlHeaderPrefix = '### YamlMime:TS';
+export const alreadyExistFilename = ["index"]
